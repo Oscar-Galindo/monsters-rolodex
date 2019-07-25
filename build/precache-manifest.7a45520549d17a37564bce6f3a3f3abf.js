5dd04c037d0883975d40b908fa73a1a73586d744
Oscar-Galindo <46630550+Oscar-Galindo@users.noreply.github.com>
2019-07-24 23:40:03 -0400

    Add files via upload

---
+++ build/precache-manifest.7a45520549d17a37564bce6f3a3f3abf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1801a830cdd806382607212511f12ac",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5cdef68538ad3ff78c7e",
+    "url": "/monsters-rolodex/static/css/main.59648786.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5cdef68538ad3ff78c7e",
+    "url": "/monsters-rolodex/static/js/main.7abe6f5b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
